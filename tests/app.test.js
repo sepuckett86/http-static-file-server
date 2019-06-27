5cea50a79dbdb08b3a994a3c77680af52e528066
sepuckett86 <sepuckett86@gmail.com>
2019-06-26 17:14:52 -0700

    add regex to check for .html extension in path name and account for it

--- tests/app.test.js
+++ tests/app.test.js
@@ -9,6 +9,20 @@ describe('app', () => {
         expect(res.text).toEqual(expect.stringContaining('Home Page'));
       });
   });
+  it('takes the /bananas path and returns bananas.html', () => {
+    return request(app)
+      .get('/bananas')
+      .then(res => {
+        expect(res.text).toEqual(expect.stringContaining('bananas'));
+      });
+  });
+  it('takes the /bananas.html path and returns bananas.html', () => {
+    return request(app)
+      .get('/bananas.html')
+      .then(res => {
+        expect(res.text).toEqual(expect.stringContaining('bananas'));
+      });
+  });
   it('takes paths with no html page and returns 404.html', () => {
     return request(app)
       .get('/does-not-exist')
